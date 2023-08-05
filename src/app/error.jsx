b2fa5c3de778ff8e12290c8f06fde64c5af0f1b9
Aminu Jamiu <108356712+jaamx@users.noreply.github.com>
2023-08-05 14:36:56 +0100

    Update error.jsx
    fixed

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,18 +1,14 @@
-'use client'
-import {useEffect} from 'react'
+"use client";
 
-
-function error({error,reset}) {
-    useEffect(() => {
-     console.log(error)
-    }, [error])
-    
+import { useEffect } from "react";
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
   return (
-    <div className='text-center mt-10'>
-        <h1>something went wrong</h1>
-        <button className='hover:text-amber-600' onClick={() => reset()}>try again</button>
+    <div className="text-center mt-10">
+      <h1>Something went wrong</h1>
+      <button className="hover:text-amber-600" onClick={() => reset()}>Try Again</button>
     </div>
-  )
+  );
 }
-
-export default error
